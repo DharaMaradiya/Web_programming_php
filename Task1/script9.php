bf34bebd376a42ea5045ca43537aaf36dce45656
DharaMaradiya <dharumaradiya@gmail.com>
2024-08-23 12:37:02 +0530

    Add files via upload

---
+++ Task1/script9.php
@@ -0,0 +1,11 @@
+<?php
+$Name="Dhara";
+$Age=20;
+$City="Rajkot";
+$Hobby="Reading";
+
+echo "Name:".$Name."<br>";
+echo "Age:".$Age."<br>";
+echo "City:".$City."<br>";
+echo "Hobby:".$Hobby."<br>";
+?>
